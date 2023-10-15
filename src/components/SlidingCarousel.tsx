164d56dcab0ec16105090c9c60e7c283a67e6161
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-10-16 00:46:16 +0545

    bareminimum for header and carousel

---
+++ src/components/SlidingCarousel.tsx
@@ -0,0 +1,44 @@
+import Carousel from "nuka-carousel";
+
+const SlidingCarousel = () => {
+  return (
+    <Carousel
+      defaultControlsConfig={{
+        // pagingDotsStyle: { display: "none" },
+        nextButtonStyle: { display: "none" },
+        prevButtonStyle: { display: "none" },
+      }}
+      renderBottomCenterControls={(controlProps) => (
+        <div className="mb-2 flex items-center gap-x-2">
+          {[...Array(controlProps.slideCount)].map((e, key) => (
+            <div
+              key={key}
+              onClick={() => controlProps.goToSlide(key)}
+              className={`h-1 w-6 cursor-pointer space-y-6 rounded-full opacity-20 ${
+                controlProps.currentSlide === key
+                  ? "bg-gray-900"
+                  : "bg-gray-400"
+              }  hover:bg-gray-600`}
+            ></div>
+          ))}
+        </div>
+      )}
+      autoplay={true}
+    >
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+      <img src="https://zebronics.com/cdn/shop/collections/zeb-Wireless-earbuds-banner.jpg" />
+
+    </Carousel>
+  );
+};
+export default SlidingCarousel;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import Head from "next/head";
-import { HeartIcon, ShoppingCartIcon, UserIcon } from "lucide-react";
+import {
+  HeartIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+  UserIcon,
+} from "lucide-react";
+import SlidingCarousel from "~/components/SlidingCarousel";
 
 export default function Home() {
   return (
@@ -10,30 +16,45 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="flex bg-[#231f20] place-content-start  px-4 py-10 text-white">
-        <div className="logo h-10 w-10">
-          <img src="https://static.vecteezy.com/system/resources/previews/023/654/784/non_2x/golden-logo-template-free-png.png" />
+      <div className="flex place-content-start items-center bg-[#231f20] px-4 text-white">
+        <div className="logo max-h-full max-w-full ">
+          <img src="./logo.png" className="h-20 w-20" />
         </div>
-        <div className="aside-except-logo flex flex-grow  place-content-evenly pt-2">
-          <div className="navlinks flex gap-x-2">
-            <div className="navlink"> Home</div>
-            <div className="navlink"> Category</div>
-            <div className="navlink">About</div>
+        <div className="aside-except-logo flex flex-grow place-content-evenly pt-2">
+          <div className="navlinks  hidden gap-x-6 font-bold sm:flex">
+            {/* Hide on small screens */}
+            <div className="navlink">Home</div>
+            <div className="navlink">Category</div>
+            <div className="navlink">My Orders</div>
+            <div className="navlink">More</div>
           </div>
-          <div className="search-flex flex">
-            <div className="logo">Search Icon</div>
+          <div className="search-flex  hidden sm:flex">
+            {/* Hide on small screens */}
+            <div className="logo pr-2">
+              <SearchIcon />
+            </div>
             <div className="input">
-              <input />
+              <input
+                className="rounded-full placeholder:pl-2"
+                placeholder="Search"
+              />
             </div>
           </div>
-
-          <div className="extra-icons flex gap-x-2">
-            <div className="user"> <UserIcon/> </div>
-            <div className="wishlist"><HeartIcon /> </div>
-            <div className="cart"><ShoppingCartIcon /></div>
+        </div>
+        <div className="extra-icons flex gap-x-2">
+          <div className="user">
+            <UserIcon />
+          </div>
+          <div className="wishlist">
+            <HeartIcon />
+          </div>
+          <div className="cart">
+            <ShoppingCartIcon />
           </div>
         </div>
       </div>
+
+      <SlidingCarousel />
     </>
   );
 }
