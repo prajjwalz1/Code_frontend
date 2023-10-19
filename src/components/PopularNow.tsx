5ddc34742dfec93aff2aea1b44e33f29a4d236da
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-10-20 00:09:10 +0545

    updated products

--- src/components/PopularNow.tsx
+++ src/components/PopularNow.tsx
@@ -12,7 +12,25 @@ const PopularNow = () => {
             <div className="title text-xl font-extrabold">
               Code Air Buds Pro
             </div>
-            <div className="price text-xl font-extrabold">Rs. 299</div>
+            <div className="price text-xl font-extrabold">Rs. 5999</div>
+            <div className="image w-52 pl-0 ">
+              <img src="./earpods.png" alt="" />
+            </div>
+            <div className="colors flex justify-center gap-x-1">
+              <div className="color rounded-full bg-slate-100 p-1.5"></div>
+              <div className="color rounded-full bg-gray-400 p-1.5"></div>
+              <div className="color rounded-full bg-pink-300 p-1.5"></div>
+              <div className="color rounded-full bg-purple-400 p-1.5"></div>
+            </div>
+          </div>
+        </div>
+        <div className="card rounded-2xl bg-[#E6E8E9] shadow-2xl drop-shadow-2xl">
+          <div className="card-body justify-center px-2 py-2">
+            <div className="badge text-xl font-medium text-red-600">New</div>
+            <div className="title text-xl font-extrabold">
+              Code Speaker Pro
+            </div>
+            <div className="price text-xl font-extrabold">Rs. 8999</div>
             <div className="image w-52 pl-0 ">
               <img src="./watch.png" alt="" />
             </div>
@@ -28,27 +46,9 @@ const PopularNow = () => {
           <div className="card-body justify-center px-2 py-2">
             <div className="badge text-xl font-medium text-red-600">New</div>
             <div className="title text-xl font-extrabold">
-              Code Air Buds Pro
+              Code Smart Watch
             </div>
-            <div className="price text-xl font-extrabold">Rs. 299</div>
-            <div className="image w-52 pl-0 ">
-              <img src="./watch.png" alt="" />
-            </div>
-            <div className="colors flex justify-center gap-x-1">
-              <div className="color rounded-full bg-slate-100 p-1.5"></div>
-              <div className="color rounded-full bg-gray-400 p-1.5"></div>
-              <div className="color rounded-full bg-pink-300 p-1.5"></div>
-              <div className="color rounded-full bg-purple-400 p-1.5"></div>
-            </div>
-          </div>
-        </div>
-        <div className="card rounded-2xl bg-[#E6E8E9] shadow-2xl drop-shadow-2xl">
-          <div className="card-body justify-center px-2 py-2">
-            <div className="badge text-xl font-medium text-red-600">New</div>
-            <div className="title text-xl font-extrabold">
-              Code Air Buds Pro
-            </div>
-            <div className="price text-xl font-extrabold">Rs. 299</div>
+            <div className="price text-xl font-extrabold">Rs. 6999</div>
             <div className="image w-52 pl-0 ">
               <img src="./watch.png" alt="" />
             </div>
