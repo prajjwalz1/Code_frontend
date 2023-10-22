d542c063992d3d3c89c77acc90006075facc13e8
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-10-22 11:48:17 +0545

    used apis

--- src/components/PopularNow.tsx
+++ src/components/PopularNow.tsx
@@ -1,61 +1,38 @@
-const PopularNow = () => {
+import type { Products } from "~/pages";
+
+const PopularNow = ({ products }: { products: Products }) => {
   return (
-    <div className="my-4 flex-col items-center  bg-[#F2F2F2] px-8 py-4 ">
+    <div className="my-4 flex-col  items-center  bg-[#F2F2F2] px-8 py-4 ">
       <div className=" m-4 text-center text-3xl font-extrabold lg:text-4xl">
         POPULAR
         <span className="ml-3 rounded-lg bg-red-500 p-1 text-white">Now</span>
       </div>
       <div className="mx-8 flex flex-wrap justify-evenly gap-y-2">
-        <div className="card rounded-2xl bg-[#E6E8E9] shadow-2xl drop-shadow-2xl">
-          <div className="card-body justify-center px-2 py-2">
-            <div className="badge text-xl font-medium text-red-600">New</div>
-            <div className="title text-xl font-extrabold">
-              Code Air Buds Pro
-            </div>
-            <div className="price text-xl font-extrabold">Rs. 5999</div>
-            <div className="image w-52 pl-0 ">
-              <img src="./earpods.png" alt="" />
-            </div>
-            <div className="colors flex justify-center gap-x-1">
-              <div className="color rounded-full bg-slate-100 p-1.5"></div>
-              <div className="color rounded-full bg-gray-400 p-1.5"></div>
-              <div className="color rounded-full bg-pink-300 p-1.5"></div>
-              <div className="color rounded-full bg-purple-400 p-1.5"></div>
-            </div>
-          </div>
-        </div>
-        <div className="card rounded-2xl bg-[#E6E8E9] shadow-2xl drop-shadow-2xl">
-          <div className="card-body justify-center px-2 py-2">
-            <div className="badge text-xl font-medium text-red-600">New</div>
-            <div className="title text-xl font-extrabold">Code Music Pro</div>
-            <div className="price text-xl font-extrabold">Rs. 8999</div>
-            <div className="image w-52 pl-0 ">
-              <img src="./headset.png" alt="" />
-            </div>
-            <div className="colors flex justify-center gap-x-1">
-              <div className="color rounded-full bg-slate-100 p-1.5"></div>
-              <div className="color rounded-full bg-gray-400 p-1.5"></div>
-              <div className="color rounded-full bg-pink-300 p-1.5"></div>
-              <div className="color rounded-full bg-purple-400 p-1.5"></div>
+        {products.map((product) => (
+          <div key={product.id} className="card rounded-2xl bg-[#E6E8E9] shadow-2xl drop-shadow-2xl">
+            <div className="card-body justify-center px-2 py-2">
+              <div className="badge text-xl font-medium text-red-600">New</div>
+              <div className="title text-xl font-extrabold">
+                {product.pname}
+              </div>
+              <div className="price text-xl font-extrabold">
+                Rs. {product.selling_price}
+              </div>
+              <div className="image w-52 pl-0 ">
+                <img
+                  src={`https://prajjwal1.pythonanywhere.com${product.image}`}
+                  alt=""
+                />
+              </div>
+              <div className="colors flex my-2 justify-center gap-x-1">
+                <div className="color rounded-full bg-slate-100 p-1.5"></div>
+                <div className="color rounded-full bg-gray-400 p-1.5"></div>
+                <div className="color rounded-full bg-pink-300 p-1.5"></div>
+                <div className="color rounded-full bg-purple-400 p-1.5"></div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="card rounded-2xl bg-[#E6E8E9] shadow-2xl drop-shadow-2xl">
-          <div className="card-body justify-center px-2 py-2">
-            <div className="badge text-xl font-medium text-red-600">New</div>
-            <div className="title text-xl font-extrabold">Code Smart Watch</div>
-            <div className="price text-xl font-extrabold">Rs. 6999</div>
-            <div className="image w-52 pl-0 ">
-              <img src="./watch.png" alt="" />
-            </div>
-            <div className="colors flex justify-center gap-x-1">
-              <div className="color rounded-full bg-slate-100 p-1.5"></div>
-              <div className="color rounded-full bg-gray-400 p-1.5"></div>
-              <div className="color rounded-full bg-pink-300 p-1.5"></div>
-              <div className="color rounded-full bg-purple-400 p-1.5"></div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
