effaeed36f16bcce903886c7c82325b07eb6fb18
utsavadhikari1 <irakihda.vastu@gmail.com>
2023-11-29 09:01:05 +0545

    additional information in About-us page

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,7 +1,17 @@
+import Footer from "~/components/Footer";
+
 const ContactUs = () => {
   return (
     <>
+      <div className="flex justify-center items-center">
+        <img
+          src="./footer-logo.png"
+          className=" w-6/7 h-20"
+          alt="Product Image"
+        />
+      </div>
       <div className=" text-center text-2xl">How may we help you?</div>
+      <Footer /> 
     </>
   );
 };
